a8e989ff8f4872172846b71ffa3871e86f889604
gsachin1809 <gsachin1809@gmail.com>
2017-03-22 13:18:39 +0530

    made a login authincation

---
+++ attica_gold/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace attica_gold.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult admin()
+        {
+            return View();
+        }
+        public ActionResult cse()
+        {
+            return View();
+        }
+        public ActionResult Validate()
+        {
+
+            String userName = Request["user_name"];
+            String password = Request["password"];
+
+            
+            if (String.Equals(userName, "admin") && String.Equals(password, "admin")) {
+                Response.Redirect("admin");
+            }
+            else if (String.Equals(userName, "cse") && String.Equals(password, "cse"))
+            {
+                Response.Redirect("cse");
+            }
+            else
+            {
+                Response.Redirect("index");
+            }
+            return Content("Exception Occues");
+ 
+        }
+    }
+}
