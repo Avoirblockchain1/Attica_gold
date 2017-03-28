8b28c7c0aee8ad230eb823c230a89c36be0bb6d0
gsachin1809 <gsachin1809@gmail.com>
2017-03-28 10:10:15 +0530

    employee view

---
+++ attica_gold/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace attica_gold.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult create()
+        {
+            return View();
+        }
+
+        public ActionResult store()
+        {
+
+            string first_name = Request['first_name'];
+            return Content("first_name");
+        }
+
+        public ActionResult show()
+        {
+            return View();
+        }
+                public ActionResult delete()
+        {
+            return View();
+        }
+        public ActionResult edit()
+        {
+            return View();
+        }
+        public ActionResult update()
+        {
+            return View();
+        }
+
+    }
+}
