f0847bc32aacb7040651cadfd9bcbcdf71746d6b
savitha <savithakamalon@gmail.com>
2017-03-30 17:10:50 +0530

    routing fix

--- attica_gold/App_Start/RouteConfig.cs
+++ attica_gold/App_Start/RouteConfig.cs
@@ -13,65 +13,91 @@ namespace attica_gold
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-                    /*   routes.MapRoute(
-                name: "ProfileRole",
-                url: "profile/{role}/{action}/{id}",
-                defaults: new { controller = "role",  action = "Index", id = UrlParameter.Optional }
-            );
-                        */
+            /*        routes.MapRoute(
+              name: "ProfileRole",
+              url: "profile/{controller}/{action}/{id}"                
+          );*/
 
 
-            routes.MapRoute(
-                name: "cse",
-                url: "profile/{controller}/{action}/{id}",
-                defaults: new { controller = "Cse", action = "Index", id = UrlParameter.Optional }
-            );
 
-            routes.MapRoute(
-               name: "admin",
-               url: "profile/{controller}/{action}/{id}",
-               defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
-           );
+            /*   routes.MapRoute(
+                   name: "cse",
+                   url: "profile/{controller}/{action}/{id}",
+                   defaults: new { controller = "Cse", action = "Index", id = UrlParameter.Optional }
+               );
 
-             routes.MapRoute(
-                name: "BranchManager",
-                url: "profile/{controller}/{action}/{id}",
-                defaults: new { controller = "BranchManager", action = "Index", id = UrlParameter.Optional }
-            );
+               routes.MapRoute(
+                  name: "admin",
+                  url: "profile/{controller}/{action}/{id}",
+                  defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+              );
 
+                routes.MapRoute(
+                   name: "BranchManager",
+                   url: "profile/{controller}/{action}/{id}",
+                   defaults: new { controller = "BranchManager", action = "Index", id = UrlParameter.Optional }
+               );
+
+
+
+               routes.MapRoute(
+                   name: "csebranch",
+                   url: "profile/Cse/{controller}/{action}/{id}",
+                   defaults: new { controller = "Branch", action = "Index", id = UrlParameter.Optional }
+               );
+
+               routes.MapRoute(
+                   name: "csecustomer",
+                   url: "profile/cse/{controller}/{action}/{id}",
+                   defaults: new { controller = "Customer", action = "Index", id = UrlParameter.Optional }
+               );
+
+          */
             
 
             routes.MapRoute(
-                name: "csebranch",
-                url: "profile/Cse/{controller}/{action}/{id}",
-                defaults: new { controller = "Branch", action = "Index", id = UrlParameter.Optional }
-            );
+
+               name: "profilerole",
+               url: "profile/{controller}/{action}",
+               defaults: new {  action = "Index" }
+           );
 
             routes.MapRoute(
-                name: "csecustomer",
-                url: "profile/cse/{controller}/{action}/{id}",
-                defaults: new { controller = "Customer", action = "Index", id = UrlParameter.Optional }
-            );
+                 name: "roleitem",
+                 url: "profile/{role}/{controller}/{action}"
+             );
 
-                      
+
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+               url: "Login",
+               defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+           );
+
+
+            routes.MapRoute(
+
+               name: "DefaultV",
+               url: "Login/validate",
+
+               defaults: new { controller = "Login", action = "validate", id = UrlParameter.Optional }
             );
 
 
+
+
+
         }
 
-      /*  public class UserNameConstraint : IRouteConstraint
-        {
-            public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
-            {
-                List<string> users = new List<string> { "admin", "branchmn", "cse" };
-                throw new NotImplementedException();
-            }
-        }
-        */
+        /*  public class UserNameConstraint : IRouteConstraint
+          {
+              public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+              {
+                  List<string> users = new List<string> { "admin", "branchmn", "cse" };
+                  throw new NotImplementedException();
+              }
+          }
+          */
     }
 }
  
