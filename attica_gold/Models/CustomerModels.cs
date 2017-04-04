2e86a9d761f13fa3125a871bb07232b3069d6883
gsachin1809 <gsachin1809@gmail.com>
2017-04-04 15:40:07 +0530

    coustomer curd operation

--- attica_gold/Models/CustomerModels.cs
+++ attica_gold/Models/CustomerModels.cs
@@ -2,27 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Configuration;
 using System.Data.Entity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace attica_gold.Models
 {
-
-    [Table("tblEmployees")]
-    public class EmployeeModels
+    [Table("tblCustomers")]
+    public class CustomerModels
     {
         [Key]
         public int id { get; set; }
-        public string employee_id { get; set; }
         public string first_name { get; set; }
         public string last_name { get; set; }
         public string gender { get; set; }
         public string mobile_number { get; set; }
         public string alter_mobile_number { get; set; }
-        public string email { get; set; }
         public string dob { get; set; }
+        public string email { get; set; }
         public string street { get; set; }
         public string city { get; set; }
         public string state { get; set; }
@@ -32,13 +29,9 @@ namespace attica_gold.Models
         public string createdat { get; set; }
         public string modifiedat { get; set; }
         public string deletedat { get; set; }
-
     }
-    public class EmployeeContext : DbContext
+    public class CustomerContext : DbContext
     {
-        public DbSet<EmployeeModels> employee { get; set; }
+        public DbSet<CustomerModels> customer { get; set; }
     }
-
-   
-
 }
---
+++ attica_gold/Models/LoginModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace attica_gold.Models
+{
+    [Table("tblLogin")]
+    public class LoginModels
+    {
+        [Key]
+        public int id { get; set; }
+        public string username { get; set; }
+        public string user_password { get; set; }
+        public string employee_id { get; set; }
+        public string createdat { get; set; }
+        public string modifiedat { get; set; }
+        public string deletedat { get; set; }
+    }
+
+    public class LoginContext : DbContext
+    {
+        public DbSet<LoginModels> login { get; set; }
+    }
+}
